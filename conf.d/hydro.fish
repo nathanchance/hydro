5ac475822f7a98917d0da12469c410ed124a7e20
Nathan Chancellor <nathan@kernel.org>
2021-05-19 12:59:10 -0700

    conf.d/hydro.fish: Rename 'info' variable to 'dirty'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- conf.d/hydro.fish
+++ conf.d/hydro.fish
@@ -79,7 +79,7 @@ function _hydro_prompt --on-event fish_prompt
         test -z \"\$$_hydro_git\" && set --universal $_hydro_git \"\$branch \"
 
         ! command git diff-index --quiet HEAD 2>/dev/null ||
-            count (command git ls-files --others --exclude-standard (command git rev-parse --show-toplevel)) >/dev/null && set info \"$hydro_symbol_git_dirty\"
+            count (command git ls-files --others --exclude-standard (command git rev-parse --show-toplevel)) >/dev/null && set dirty \"$hydro_symbol_git_dirty\"
 
         for fetch in $hydro_fetch false
             command git rev-list --count --left-right @{upstream}...@ 2>/dev/null |
@@ -95,7 +95,7 @@ function _hydro_prompt --on-event fish_prompt
                     set upstream \" $hydro_symbol_git_ahead \$ahead $hydro_symbol_git_behind \$behind\"
             end
 
-            set --universal $_hydro_git \"\$branch\$info\$upstream \"
+            set --universal $_hydro_git \"\$branch\$dirty\$upstream \"
 
             test \$fetch = true && command git fetch --no-tags 2>/dev/null
         end
